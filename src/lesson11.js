50b42e64114ee8011807c93d662bf66d7f4f5821
wenbo <18629531510@163.com>
2020-08-02 17:16:33 +0800

    文件名修改

--- src/lesson11.js
+++ src/lesson11.js
@@ -1,50 +1,73 @@
-class Subject {
-    constructor () {
-        this.state = 0
-        this.observers = []
-    }
+// class Iterator {
+//     constructor (container) {
+//         this.list = container.list
+//         this.index = 0
+//     }
 
-    getState() {
-        return this.state
-    }
+//     next() {
+//         if (this.hasNext()) {
+//             return this.list[this.index++]
+//         }
+//         return null
 
-    setState(state) {
-        this.state = state
-        this.notifyAllObserver()
-    }
+//     }
 
-    notifyAllObserver() {
-        this.observers.forEach(observer => {
-            observer.update()
-        })
-    }
+//     hasNext() {
+//         if (this.index >= this.list.length) {
+//             return false
+//         }
+//         return true
+//     }
+// }
 
-    attach(observer) {
-        this.observers.push(observer)
+
+// class Container {
+//     constructor (list) {
+//         this.list = list
+//     }
+
+//     getIterator() {
+//         return Iterator(this)
+//     }
+// }
+
+// var arr = [1, 2, 3, 4, 5]
+// const container = new Container(arr)
+// const iterator = new Iterator(container)
+// while (iterator.hasNext()) {
+//     console.log(iterator.next());
+// }
+
+// function each(data) {
+//     //生成迭代器
+//     let iterator = data[Symbol.iterator]()
+// console.log(iterator.next()); // 有数据时返回 {value: 1, done: false}
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next()); // 无数据时返回 {value: undefined, done: true}
+
+//     let item = { done: false }
+//     while (!item.done) {
+//         item = iterator.next()
+//         if (!item.done) {
+//             console.log(item.value);
+//         }
+//     }
+// }
+
+function each(data) {
+    // 必须带有遍历器特性
+    for (const item of data) {
+        console.log(item);
     }
 }
+let arr = [1, 2, 3, 4]
+let nodeList = document.getElementsByTagName("h3")
+let m = new Map()
+m.set('a', 100)
+m.set('b', 200)
 
-
-class Observer {
-    constructor (name, subject) {
-        this.name = name
-        this.subject = subject
-        this.subject.attach(this)
-    }
-
-    update() {
-        console.log(`${this.name} update, state: ${this.subject.getState()}`);
-    }
-}
-
-let s = new Subject()
-let o1 = new Observer('o1', s)
-let o2 = new Observer('o2', s)
-let o3 = new Observer('o3', s)
-
-s.setState(1)
-
-s.setState(2)
-
-s.setState(3)
-
+each(arr)
+each(nodeList)
+each(m)
--- src/lesson12.js
+++ src/lesson12.js
@@ -1,73 +1,49 @@
-// class Iterator {
-//     constructor (container) {
-//         this.list = container.list
-//         this.index = 0
-//     }
+// 状态
+class State {
+    constructor (color) {
+        this.color = color
+    }
 
-//     next() {
-//         if (this.hasNext()) {
-//             return this.list[this.index++]
-//         }
-//         return null
-
-//     }
-
-//     hasNext() {
-//         if (this.index >= this.list.length) {
-//             return false
-//         }
-//         return true
-//     }
-// }
-
-
-// class Container {
-//     constructor (list) {
-//         this.list = list
-//     }
-
-//     getIterator() {
-//         return Iterator(this)
-//     }
-// }
-
-// var arr = [1, 2, 3, 4, 5]
-// const container = new Container(arr)
-// const iterator = new Iterator(container)
-// while (iterator.hasNext()) {
-//     console.log(iterator.next());
-// }
-
-// function each(data) {
-//     //生成迭代器
-//     let iterator = data[Symbol.iterator]()
-// console.log(iterator.next()); // 有数据时返回 {value: 1, done: false}
-// console.log(iterator.next());
-// console.log(iterator.next());
-// console.log(iterator.next());
-// console.log(iterator.next()); // 无数据时返回 {value: undefined, done: true}
-
-//     let item = { done: false }
-//     while (!item.done) {
-//         item = iterator.next()
-//         if (!item.done) {
-//             console.log(item.value);
-//         }
-//     }
-// }
-
-function each(data) {
-    // 必须带有遍历器特性
-    for (const item of data) {
-        console.log(item);
+    handle(context) {
+        console.log(`turn to ${this.color} light`);
+        // 设置状态
+        context.setState(this)
     }
 }
-let arr = [1, 2, 3, 4]
-let nodeList = document.getElementsByTagName("h3")
-let m = new Map()
-m.set('a', 100)
-m.set('b', 200)
 
-each(arr)
-each(nodeList)
-each(m)
+// 主体
+class Context {
+    constructor () {
+        this.state = null
+    }
+
+    // 获取状态
+    getState() {
+        return this.state
+    }
+
+    setState(state) {
+        this.state = state
+    }
+}
+
+
+// test
+
+let context = new Context()
+
+let green = new State("green")
+let yellow = new State("yellow")
+let red = new State("red")
+
+// 绿灯亮了
+green.handle(context)
+console.log(context.getState());
+
+// 黄灯亮了
+yellow.handle(context)
+console.log(context.getState());
+
+// 红灯亮了
+red.handle(context)
+console.log(context.getState());
